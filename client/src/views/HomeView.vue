63adb922b997a32abea599a55c386b5da620564c
aydawka <aydan.gasimova2@gmail.com>
2023-09-23 23:51:07 -0700

    feat: client initiation

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
     Finance tracker app
-    <TheWelcome />
+
   </main>
 </template>
