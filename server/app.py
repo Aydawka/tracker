99afb8459e2a8240f26d94cccd3fab0ec02acb5c
aydawka <aydan.gasimova2@gmail.com>
2023-09-26 23:52:31 -0700

    wip: client initiation

---
+++ server/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_cors import CORS
+
+
+def create_app():
+    """Initialize the core application."""
+    app = Flask(__name__)
+    return app
+
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-p", "--port", default=5000, type=int, help="port to listen on"
+    )
+    args = parser.parse_args()
+    port = args.port
+
+    app = create_app()
+
+    app.run(host="0.0.0.0", port=port)
